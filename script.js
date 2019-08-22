925ed4110721c050abdc16fe203e5ae4d8b9144f
TuanNguyenVanAnh <nvatuan3@gmail.com>
2019-08-22 15:26:08 +0700

    optimize

--- script.js
+++ script.js
@@ -11,183 +11,83 @@ var i = 0;
 
 // declare radio choises
 var rad = [];
-rad[0] = document.getElementById("rad-zero");
-rad[1] = document.getElementById("rad-one");
-rad[2] = document.getElementById("rad-two");
-rad[3] = document.getElementById("rad-three");
-rad[4] = document.getElementById("rad-four");
+// rad[0] = document.getElementById("rad-zero");
+// rad[1] = document.getElementById("rad-one");
+// rad[2] = document.getElementById("rad-two");
+// rad[3] = document.getElementById("rad-three");
+// rad[4] = document.getElementById("rad-four");
+
+for (j = 0; j <= 4; j++) {
+	rad[j] = document.getElementById("rad" + j);
+}
+
 
 
 // declare what number index of image present for what image
-image[0] = "images/image1.jpg";
-image[1] = "images/image2.jpg";
-image[2] = "images/image3.jpg";
-image[3] = "images/image4.jpg";
-image[4] = "images/image5.jpg";
+// image[0] = "images/image1.jpg";
+// image[1] = "images/image2.jpg";
+// image[2] = "images/image3.jpg";
+// image[3] = "images/image4.jpg";
+// image[4] = "images/image5.jpg";
 
-// declare first picture is image1.jpg
+for (var j = 0; j <= 4; j++) {
+	image[j] = "images/image" + (j + 1) + ".jpg";
+}
+
 mainPic.src = image[0];
 
+// declare first picture is image1.jpg
+// mainPic.src = image[0];
+
 // click to right and left button to change picture
+
 back.addEventListener("click", function() {
+	i = (i+4) %5;
+	console.log(i);
 	
-	switch (i) {
-		case 0:
-			i = 4;
-			rad[0],[1],[2],[3].checked = false;
-			rad[4].checked = true;
-			break;
-		case 1:
-			i = 0;
-			rad[1],[4],[2],[3].checked = false;
-			rad[0].checked = true;
-			break;
-		case 2:
-			i = 1;
-			rad[0],[4],[2],[3].checked = false;
-			rad[1].checked = true;
-			break;
-		case 3:
-			i = 2;
-			rad[0],[1],[4],[3].checked = false;
-			rad[2].checked = true;
-			break;
-		case 4:
-			i = 3;
-			rad[0],[1],[2],[4].checked = false;
-			rad[3].checked = true;
-			break;
-	}
+	rad[i].checked = true;
+
 	mainPic.src = image[i];
 });
 next.addEventListener("click", function() {
+	i = (i+1)%5;
 
-	switch (i) {
-		case 0:
-			i = 1;
-			rad[0],[3],[2],[4].checked = false;
-			rad[1].checked = true;
-			break;
-		case 1:
-			i = 2;
-			rad[0],[1],[3],[4].checked = false;
-			rad[2].checked = true;
-			break;
-		case 2:
-			i = 3;
-			rad[0],[1],[2],[4].checked = false;
-			rad[3].checked = true;
-			break;
-		case 3:
-			i = 4;
-			rad[0],[1],[2],[3].checked = false;
-			rad[4].checked = true;
-			break;
-		case 4:
-			i = 0;
-			rad[3],[1],[2],[4].checked = false;
-			rad[0].checked = true;
-			break;
-	}
+	console.log(i);
+	
+	rad[i].checked = true;
 
 	mainPic.src = image[i];
 });
 
-// click to radio to change picture
+// // click to radio to change picture
 
-rad[0].addEventListener("click", function () {
-	rad[3],[1],[2],[4].checked = false;
-	rad[0].checked = true;
-	mainPic.src = image[0];
-});
+for (var j = 0; j <= 4; j++) {
+	rad[j].addEventListener("click", function() {
+		this.checked = true;
+		mainPic.src = image[this.id.substring(3,4)];
+	});
+}
 
-rad[1].addEventListener("click", function () {
-	rad[3],[0],[2],[4].checked = false;
-	rad[1].checked = true;
-	mainPic.src = image[1];
-});
 
-rad[2].addEventListener("click", function () {
-	rad[3],[1],[0],[4].checked = false;
-	rad[2].checked = true;
-	mainPic.src = image[2];
-});
 
-rad[3].addEventListener("click", function () {
-	rad[0],[1],[2],[4].checked = false;
-	rad[3].checked = true;
-	mainPic.src = image[3];
-});
-
-rad[4].addEventListener("click", function () {
-	rad[3],[1],[2],[0].checked = false;
-	rad[4].checked = true;
-	mainPic.src = image[4];
-});
-
-// catch left arrow and right arrow 
+// // catch left arrow and right arrow 
 document.addEventListener("keyup", function(e) {
 	if (e.keyCode === 37) {
-		switch (i) {
-			case 0:
-				i = 4;
-				rad[0],[1],[2],[3].checked = false;
-				rad[4].checked = true;
-				break;
-			case 1:
-				i = 0;
-				rad[1],[4],[2],[3].checked = false;
-				rad[0].checked = true;
-				break;
-			case 2:
-				i = 1;
-				rad[0],[4],[2],[3].checked = false;
-				rad[1].checked = true;
-				break;
-			case 3:
-				i = 2;
-				rad[0],[1],[4],[3].checked = false;
-				rad[2].checked = true;
-				break;
-			case 4:
-				i = 3;
-				rad[0],[1],[2],[4].checked = false;
-				rad[3].checked = true;
-				break;
-		}
+		i = (i+4) %5;
+		console.log(i);
+		
+		rad[i].checked = true;
+
 		mainPic.src = image[i];
+
 	}
 });
 
 document.addEventListener("keyup", function(e) {
 	if (e.keyCode === 39) {
-		switch (i) {
-			case 0:
-				i = 1;
-				rad[0],[3],[2],[4].checked = false;
-				rad[1].checked = true;
-				break;
-			case 1:
-				i = 2;
-				rad[0],[1],[3],[4].checked = false;
-				rad[2].checked = true;
-				break;
-			case 2:
-				i = 3;
-				rad[0],[1],[2],[4].checked = false;
-				rad[3].checked = true;
-				break;
-			case 3:
-				i = 4;
-				rad[0],[1],[2],[3].checked = false;
-				rad[4].checked = true;
-				break;
-			case 4:
-				i = 0;
-				rad[3],[1],[2],[4].checked = false;
-				rad[0].checked = true;
-				break;
-		}
+		i = (i+1) % 5;
+
+		rad[i].checked = true;
 
 		mainPic.src = image[i];
 	}
